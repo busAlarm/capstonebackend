f7202f54e13e716c756bbea738023ff6a87d58eb
= <79920930+ji-hyeon97@users.noreply.github.com>
2023-03-29 11:36:36 +0900

    feat : initial commit

---
+++ src/main/java/capstone/busAlarm/BusAlarmApplication.java
@@ -0,0 +1,13 @@
+package capstone.busAlarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusAlarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusAlarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/busAlarm/BusAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.busAlarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusAlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
